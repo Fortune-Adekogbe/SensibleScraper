b06e74469637ee5daecac55682eb6ce7ed0d7ab4
Fortune Adekogbe <fortuneadekogbe@gmail.com>
2023-03-06 16:51:32 +0000

    updated document reference in scraper.py

--- scraper.py
+++ scraper.py
@@ -5,6 +5,7 @@ import requests
 
 API_KEY = os.getenv('API_KEY')
 
+
 def extract_content(d_type: str, d_name: str, env: str):
     """
     Extract content from a PDF file.
@@ -19,10 +20,11 @@ def extract_content(d_type: str, d_name: str, env: str):
     with open(d_name, 'rb') as fp:
         pdf_file = fp.read()
         response = requests.post(url, headers=headers, data=pdf_file)
-        
+
         print(f"Status code: {response.status_code}")
         if response.status_code == 200:
-            print(json.dumps(response.json(), indent=2))            
+            print(json.dumps(response.json(), indent=2))
+
 # 1. Extract content from a structured PDF file
 document_type = "tax_forms"
 document_name = "senior_1040_2019_sample.pdf"
